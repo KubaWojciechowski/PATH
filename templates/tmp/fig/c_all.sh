309e283d38b4a6edc34ab533116bb5539b796ade
KubaWojciechowski <kubwoj95@gmail.com>
2019-12-01 15:34:22 +0100

    cleanup

--- templates/tmp/fig/c_all.sh
+++
@@ -1,6 +0,0 @@
-for INP in *.tga
-do
-newname=`basename $INP .tga`
-convert $INP $newname.png
-done
-echo ” process ended, please check your graphical files”
--- templates/tmp/getProt.sh
+++
@@ -1,5 +0,0 @@
-name=$1
-sed -i '/TER   /d' ./$name
-sed -i '/MODEL /d' ./$name
-sed -i '/ENDMDL/d' ./$name
-vmd $name -dispenv text -e getProt.tcl
